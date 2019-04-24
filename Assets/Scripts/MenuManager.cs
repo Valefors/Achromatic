cec89e2eb3b15b2645070fb9a2f848ba8a3c06b1
Valefor <emeline.berenguier@gmail.com>
2019-04-24 15:13:37 +0200

    [ADD] Animation for buttons

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
