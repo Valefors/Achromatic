c2a142930a19d64b308c9300f59931fd41315f72
Valefor <emeline.berenguier@gmail.com>
2019-04-24 17:21:54 +0200

    [ADD] Menu transition

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,6 +5,42 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    RectTransform _currentPanel;
+    RectTransform _selectedPanel;
+    [SerializeField] RectTransform _menuPanel;
+    [SerializeField] RectTransform _creditsPanel;
+
+    private void Start()
+    {
+        _currentPanel = _menuPanel;
+    }
+
+    public void OnBack()
+    {
+        _selectedPanel = _menuPanel;
+        DisableCurrentScreen();
+    }
+
+    public void OnCredits()
+    {
+        _selectedPanel = _creditsPanel;
+        DisableCurrentScreen();
+    }
+
+    void DisableCurrentScreen()
+    {
+        StartCoroutine(StaticFunctions.FadeOut(result => _currentPanel.GetComponent<CanvasGroup>().alpha = result, 1f, ActivateCurrentScreen));
+    }
+
+    void ActivateCurrentScreen()
+    {
+        _currentPanel.gameObject.SetActive(false);
+        _currentPanel = _selectedPanel;
+        _selectedPanel.gameObject.SetActive(true);
+        StartCoroutine(StaticFunctions.FadeIn(result => _currentPanel.GetComponent<CanvasGroup>().alpha = result, 1f, null));
+        _selectedPanel = null;
+    }
+
     // Start is called before the first frame update
     public void OnApplicationQuit()
     {
