678fc6e8b3f745dcdef45564cddfa0eee3329c48
Valefor <emeline.berenguier@gmail.com>
2019-05-13 12:12:49 +0200

    [ADD] Animation Wardrobe

---
+++ Assets/Scripts/ObjectManipulation/Objects/Wardrobe.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Outline))]
+[RequireComponent(typeof(Animator))]
+public class Wardrobe : Interactable
+{
+    Animator _animator;
+    bool _isFirstOpen = false;
+    BoxCollider _collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+        _interactionName = Utils.OPEN_INTERACTION;
+        _animator = GetComponent<Animator>();
+        _collider = GetComponent<BoxCollider>();
+    }
+
+    public override void SetModeHoover()
+    {
+        base.SetModeHoover();
+    }
+
+    public override void SetInteractionMode()
+    {
+        if (_isFirstOpen) return;
+
+        base.SetInteractionMode();
+
+        if (!_isFirstOpen)
+        {
+            _isFirstOpen = true;
+            OpenWardrobe();
+            SetModeNormal();
+            Destroy(this.GetComponent<Wardrobe>());
+            Destroy(this.GetComponent<Outline>());
+
+            if (_collider != null) _collider.enabled = false;
+        }
+        
+    }
+
+    void OpenWardrobe()
+    {
+        if (_animator != null) _animator.SetBool("IsOpen", true);
+    }
+}
