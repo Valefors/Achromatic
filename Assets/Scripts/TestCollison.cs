8355fc71b720fa0656989ac22aaecbdd63e0142f
Romane <romane.l.garcia@gmail.com>
2019-05-22 17:03:07 +0200

    [UPDATE] Light fix

---
+++ Assets/Scripts/TestCollison.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollison : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+    }
+}
