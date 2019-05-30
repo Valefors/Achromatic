e1dfaf763724bb015c9f295dec7eb761c7e5ab50
Valefor <emeline.berenguier@gmail.com>
2019-05-30 12:17:42 +0200

    [ADD] Random Lights

---
+++ Assets/Scripts/ObjectManipulation/Objects/Cars.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    int counter = 0;
+    int playRate = 0;
+    int PLAY_RATE_MIN = 150;
+    int PLAY_RATE_MAX = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayAnimation();
+    }
+
+    // Update is called once per frame
+    void PlayAnimation()
+    {
+        _animator.Play("CarLight", -1, 0f);
+    }
+
+    private void Update()
+    {
+        counter++;
+
+        if(counter >= playRate)
+        {
+            PlayAnimation();
+            counter = 0;
+            playRate = Random.Range(PLAY_RATE_MIN, PLAY_RATE_MAX);
+            print(playRate);
+        }
+    }
+    
+}
